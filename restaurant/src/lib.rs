64f869fc1ca5e102e1da7c81b358ff422073b494
anborong <anbr@novastar.tech>
2021-12-09 14:50:35 +0800

    finish chapter7 mod

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,35 +1,9 @@
-mod front_of_house {
-    fn hello () {}
+mod front_of_house;
+mod great;
 
-    pub mod hosting {
-        pub fn add_to_waitlist() {
-            // 放稳同一个包下的方法
-            seat_at_table();
-            super::serving::take_order();
-            super::hello();
-        }
+pub use crate::front_of_house::hosting;
+pub use crate::great::amber;
 
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        pub fn take_order() {}
-
-        fn server_order() {}
-
-        fn take_payment() {}
-    }
-}
-
-
-pub fn eat_at_restaurant () {
-    // 绝对路径
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // 相对路径
-    front_of_house::hosting::add_to_waitlist();
-
-}
 
 #[cfg(test)]
 mod tests {
@@ -37,10 +11,5 @@ mod tests {
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn test_front_of_house() {
-        crate::front_of_house::hosting::add_to_waitlist()
     }
 }
